9c541d5aa34e5a4377dcead4ce45995859aee00c
井本竜 <>
2020-02-13 21:45:25 +0900

    ユーザー側のコントローラを用意　※後で修正する可能性あり

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,28 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// ユーザー側
+// Route::get('login','User\Auth\LoginController@showLoginForm')->name('user.login');
+// Route::post('login','User\Auth\LoginController@login');
+
+// Route::get('logout','User\Auth\LoginController@logout')->name('user.logout');
+
+// Route::middleware('auth:user')->group(function () { 
+//     Route::get('/','User\TopController@index')->name('user.top');
+
+    
+
+
+// });
+
+
+
+
+
+// Route::middleware('auth:user')->group(function(){
+
+// });
+
+
+// 管理側
