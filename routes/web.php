b0735c7571b00f20785a28d696903cc1d1d15642
井本竜 <>
2020-02-13 22:01:15 +0900

    ユーザー側のroute 書き込んだ

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,28 @@ Route::get('/home', 'HomeController@index')->name('home');
 // Route::get('logout','User\Auth\LoginController@logout')->name('user.logout');
 
 // Route::middleware('auth:user')->group(function () { 
-//     Route::get('/','User\TopController@index')->name('user.top');
+    // Route::get('/','User\TopController@index')->name('user.top');
 
+    // Route::get('/search/a8','User\A8SearchController@index')->name('user.a8');
+
+    // Route::get('/search/rakuten','User\RakutenSearchController@index')->name('user.rakuten');
+
+    // Route::get('/search/amazon','User\AmazonSearchController@index')->name('user.amazon');
+
+    // Route::get('/search/yahoo','User\YahooSearchController@index')->name('user.yahoo');
+
+    // Route::get('/items','User\ItemListController@index')->name('user.item_list');
+
+    // Route::get('/items/register','User\ItemRegisterController@index')->name('user.item_register');
+
+    // Route::get('/items/{item_id}','User\ItemDetailsController@index')->name('user.item_details');
+
+    // Route::get('/sales','User\SalesAggregateController@index')->name('user.sales_aggregate');
+
+    // Route::get('/sales/{date}','User\SalesMonthlyAggregationController@index')->name('user.sales_monthly_aggregation');
+
+    // Route::get('/acount','User\AcountEditController@index')->name('user.acount_edit');
     
-
 
 // });
 
